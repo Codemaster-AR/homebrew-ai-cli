ab74c1db53e3b3688aca1f7889f461c087e0fcd8
Anay Rustogi <codemaster.ar@gmail.com>
2026-03-01 16:10:21 +0400

    Update ai-cli.rb

--- Formula/ai-cli.rb
+++ Formula/ai-cli.rb
@@ -3,7 +3,7 @@ class AiCli < Formula
 
   desc "A Terminal AI Agent running on Groq"
   homepage "https://github.com/Anay-Rustogi/ai-cli"
-  url "https://github.com/codemaster-ar/ai-cli/archive/refs/tags/v4.0.0.tar.gz"
+  url "https://github.com/codemaster-ar/ai-cli/archive/refs/tags/v5.0.0.tar.gz"
   sha256 "a883e037aaa21cea316c6d46b2396d16df2144ec715261f21b99ea03032fd2ac"
   license "MIT"
 
